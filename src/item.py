bb019a70864382b1e7c29e8087da1a926e86b34c
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-15 10:00:29 -0600

    create LightSource subclass

--- src/item.py
+++ src/item.py
@@ -8,3 +8,11 @@ class Item:
 
     def on_drop(self):
         print(f"\nYou have dropped {self.name}")
+
+
+class LightSource(Item):
+    def __init__(self, name, description):
+        super().__init__(name, description)
+
+    def on_drop(self):
+        print(f"\nIt's not wise to drop your source of light!")
